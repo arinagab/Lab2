c04fbae22056f6cca5c91b897c343684d8c88289
Arina Gabulyan <arinagab@yandex.ru>
2016-10-07 15:06:53 +0300

    Update arr_algs.py

--- arr_algs.py
+++ arr_algs.py
@@ -1,24 +1,19 @@
 mas = [4, 17, 2, 3, 1040, 256, 90, 18]
 
-
 def poisk_min(mas):
-    i = 0
-    min = mas[0]
-    while i < len(mas):
-        if mas[i] < min:
-            min = i
-            min = mas[i]
-        i = i + 1
-    return min
-
+	min = mas[0]  
+	for z in mas: 
+		if z < min: 
+			min=z
+	return min
+ 
 
 def sred_mas(mas):
-    s = 0
-    for z in mas:
-        s = s + z
-    s = s / len(mas)
-    return s
-
-
-print ("1) Минимум в массиве:", (poisk_min(mas)))
-print ("2) Среднее арифметическое в массиве:", (sred_mas(mas)))
+	s=0
+	for z in mas: 
+		s = s + z
+	s = s / len(mas)
+	return s
+	
+print ("1) Минимум в массиве:",(poisk_min(mas))) 	
+print ("2) Среднее арифметическое в массиве:",(sred_mas(mas)))  
